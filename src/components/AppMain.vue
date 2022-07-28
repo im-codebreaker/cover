22c62675cc1b1b916b4bbd96c52e8e9c68f2541a
MehdiHan <meh.bouchard@outlook.fr>
2022-07-28 21:29:08 +0200

    chore: add filterByFollower and filterByPopularity methods

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,15 +1,23 @@
 <template>
-  <main class="container">
-    <h1>Search for your artist</h1>
-    <section>
+  <main class="main">
+    <section class="container">
+      <div class="hello">
+        <h1>Welcome on _Cover</h1>
+        <p
+          >Try _Cover to search and discover many music Artists, try the search
+          engine to display Albums and Top Tracks</p
+        >
+      </div>
+      <button @click="filterByFollower">Most followed</button>
+      <button @click="filterByPopularity">Most Popular</button>
       <ul class="search__list">
         <li
           class="search__list-item"
           v-for="artists in artistsList"
           :key="artists.id"
         >
-          {{ artists.name }} - {{}}
-          <figure>
+          <h2 class="list__item-title">{{ artists.name }} </h2>
+          <figure class="list__item-image">
             <img
               v-if="artists?.images[0]?.url === undefined"
               class="cover"
@@ -38,6 +46,18 @@ export default {
       artistsList: [],
     };
   },
+  methods: {
+    filterByFollower() {
+      this.artistsList = this.artistsList.sort(function (a, b) {
+        return b.followers.total - a.followers.total;
+      });
+    },
+    filterByPopularity() {
+      this.artistsList = this.artistsList.sort(function (a, b) {
+        return b.popularity - a.popularity;
+      });
+    },
+  },
   mounted() {
     (async () => {
       const data = await searchItems(this.$store.state.favoriteArtist);
@@ -52,10 +72,33 @@ export default {
   margin: auto;
   max-width: 1320px;
 }
+.main {
+  margin-top: 120px;
+}
+.hello {
+  padding: 40px;
+  background-color: #12121228;
+  border-radius: 8px;
+}
 .search__list {
   display: grid;
   grid-template-columns: 1fr;
-  gap: 20px;
+  gap: 10px;
+  margin: 120px 0 240px 0;
+}
+.list__item-title {
+  font-size: 16px;
+}
+.list__item-image {
+  overflow: hidden;
+  border-radius: 8px;
+}
+.list__item-image img {
+  transition: transform 500ms ease-out;
+}
+.list__item-image:hover img {
+  transform: scale(1.05);
+  transition: transform 500ms ease-out;
 }
 .cover {
   width: 240px;
@@ -64,7 +107,7 @@ export default {
 }
 @media screen and (min-width: 64em) {
   .search__list {
-    grid-template-columns: 320px 320px 320px 320px;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
   }
   .cover {
     width: 320px;
